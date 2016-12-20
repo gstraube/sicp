df5289f49fe888dd9e99ce2548ded9fd5988c005
gstraube <gstraube@mailbox.org>
2016-12-20 17:55:09 +0100

    Use improved good-enough? procedure when calculating square roots

---
+++ chapter_1/ex7.scm
@@ -0,0 +1,16 @@
+(define (square x) (* x x))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (good-enough? new-guess old-guess)
+  (< (/ (abs (- new-guess old-guess)) new-guess) 0.001))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? (improve guess x) guess)
+    guess
+    (sqrt-iter (improve guess x)
+	       x)))
