1fcfe894403a294939fb69f0fe60f69627891830
gstraube <gstraube@mailbox.org>
2016-12-21 18:37:38 +0100

    Write the same procedure twice yielding once a recursive process and once an iterative process

---
+++ chapter_1/ex11.scm
@@ -0,0 +1,18 @@
+(define (f-rec n)
+  (if (< n 3)
+    n
+    (+ (f-rec (- n 1))
+       (* 2 (f-rec (- n 2)))
+       (* 3 (f-rec (- n 3))))))
+
+
+(define (f-tail-rec n a b c)
+  (if (= n 0)
+    c
+    (f-tail-rec
+      (- n 1)
+      (+ (* 3 c) (* 2 b) a)
+      a
+      b)))
+
+(define (f-iter n) (f-tail-rec n 2 1 0))
