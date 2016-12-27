733f847aec15f5277d2b817d49f58441344aa4cf
gstraube <gstraube@mailbox.org>
2016-12-27 21:17:29 +0100

    Add missing parentheses

--- chapter_1/ex12.scm
+++ chapter_1/ex12.scm
@@ -1,4 +1,4 @@
-﻿(define (binom n k)
+(define (binom n k)
   (cond ((= k 0) 1)
 	((= n k) 1)
-	(else (+ (binom (- n 1) k) (binom (- n 1) (- k 1)))
+	(else (+ (binom (- n 1) k) (binom (- n 1) (- k 1))))))
