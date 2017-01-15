434f8aad6bc4af33f5099d1a0628f87e24637fe2
gstraube <gstraube@mailbox.org>
2017-01-15 12:53:39 +0100

    Calculate sum of squared prime numbers for a given range

--- chapter_1/ex33.scm
+++ chapter_1/ex33.scm
@@ -6,3 +6,22 @@
 	(iter (next a) (combiner result (term a)))
 	(iter (next a) result))))
   (iter a null-value))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+	((divides? test-divisor n) test-divisor)
+	(else (find-divisor n (+ test-divisor 1)))))
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (square x) (* x x))
+
+(define (inc x) (+ x 1))
+
+(define (sum-squared-primes a b)
+  (filtered-accumulate + 0 square a inc b prime?))
