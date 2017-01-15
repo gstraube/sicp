f5ea2dc7c77f158f2d57e47f1a3895cc333b1357
gstraube <gstraube@mailbox.org>
2017-01-15 12:51:10 +0100

    Accumulate filtered list of values

---
+++ chapter_1/ex33.scm
@@ -0,0 +1,8 @@
+(define (filtered-accumulate combiner null-value term a next b filter)
+  (define (iter a result)
+    (if (> a b)
+      result
+      (if (filter a)
+	(iter (next a) (combiner result (term a)))
+	(iter (next a) result))))
+  (iter a null-value))
