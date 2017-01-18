0c5338722b28f7aed5caf54b7abce256a9300362
gstraube <gstraube@mailbox.org>
2017-01-18 17:47:26 +0100

    Find required precision

--- chapter_1/ex37.scm
+++ chapter_1/ex37.scm
@@ -1,6 +1,19 @@
+(define (close-enough? x)
+  (define phi (/ (+ 1 (sqrt 5)) 2))
+  (< (abs (- x (/ 1 phi))) 0.00001))
+
 (define (cont-frac n d k)
   (define (calc i)
     (if (= i k)
       (/ (n k) (d k))
       (/ (n i) (+ (d i) (calc (+ i 1))))))
   (calc 1))
+
+(define (find-needed-precision)
+  (define (check k)
+    (if (close-enough? (cont-frac
+			 (lambda (i) 1.0)
+			 (lambda (i) 1.0) k))
+      k
+      (check (+ k 1))))
+  (check 1))
