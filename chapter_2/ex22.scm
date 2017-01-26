6f3b5f8679ff841260c0a6628a3a84369b59afed
gstraube <gstraube@mailbox.org>
2017-01-26 20:02:50 +0100

    Create procedures to construct points and segments

---
+++ chapter_2/ex22.scm
@@ -0,0 +1,15 @@
+(define (make-point x y) (cons x y))
+(define (x-point p) (car p))
+(define (y-point p) (cdr p))
+
+(define (make-segment start end) (cons start end))
+(define (start-segment segment) (car segment))
+(define (end-segment segment) (cdr segment))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
