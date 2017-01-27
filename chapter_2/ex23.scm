9322f04f632f68214ce77baec32edfc3294d91ef
gstraube <gstraube@mailbox.org>
2017-01-27 20:16:44 +0100

    Implement a different representation of rectangles

--- chapter_2/ex23.scm
+++ chapter_2/ex23.scm
@@ -9,6 +9,9 @@
 (define (make-rectangle p1 p2 p3)
   (cons p1 (cons p2 p3)))
 
+(define (make-rectangle-seg p1 p2 p3)
+  (cons (make-segment p1 p2) (make-segment p1 p3)))
+
 (define (perimeter rectangle)
   (+ (* 2 (height rectangle)) (* 2 (width rectangle))))
 
@@ -16,10 +19,10 @@
   (* (height rectangle) (width rectangle)))
 
 (define (width rectangle)
-  (distance (car rectangle) (car (cdr rectangle))))
+  (distance (first-point rectangle) (second-point rectangle)))
 
 (define (height rectangle)
-  (distance (car rectangle) (cdr (cdr rectangle))))
+  (distance (first-point rectangle) (third-point rectangle)))
 
 (define (distance p1 p2)
   (sqrt (+ (expt (diff p1 p2 x-point) 2)
@@ -27,3 +30,19 @@
 
 (define (diff p1 p2 selector)
   (- (selector p1) (selector p2)))
+
+(define (first-point rectangle)
+  (if (point? (car rectangle))
+    (car (car rectangle))
+    (car rectangle)))
+
+(define (second-point rectangle)
+  (if (point? (car rectangle))
+    (cdr (car rectangle))
+    (car (cdr rectangle))))
+
+(define (third-point rectangle)
+  (cdr (cdr rectangle)))
+
+(define (point? component)
+  (pair? (car component)))
