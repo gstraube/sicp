17ed2ba8fa444ba72bc0e73898b209fc0f312652
gstraube <gstraube@mailbox.org>
2017-01-29 12:17:15 +0100

    Provide a procedure for adding two Church numerals

---
+++ chapter_2/ex26.scm
@@ -0,0 +1,11 @@
+(define zero (lambda (f) (lambda (x) x)))
+
+(define one (lambda (f) (lambda (x) (f x))))
+
+(define two (lambda (f) (lambda (x) (f (f x)))))
+
+(define (add-1 n)
+  (lambda (f) (lambda (x) (f ((n f) x)))))
+
+(define (add n1 n2)
+  (lambda (f) (lambda (x) ((n1 f) ((n2 f) x)))))
