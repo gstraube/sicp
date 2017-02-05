b23da501c5557601603c424c83d62b5008d88256
gstraube <gstraube@mailbox.org>
2017-02-05 16:58:19 +0100

    Apply a given procedure to every leaf in a tree

---
+++ chapter_2/ex231.scm
@@ -0,0 +1,10 @@
+#lang racket
+
+(define (tree-map proc tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (tree-map proc sub-tree)
+             (proc sub-tree)))
+       tree))
+
+(define (square-tree tree) (tree-map (lambda (x) (expt x 2)) tree))
