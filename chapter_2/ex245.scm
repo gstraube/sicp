01c01b7cfebb207cae25da2b38001e5869c3a943
gstraube <gstraube@mailbox.org>
2017-02-10 22:11:04 +0100

    Express right-split and up-split in terms of a general splitting procedure

---
+++ chapter_2/ex245.scm
@@ -0,0 +1,14 @@
+#lang sicp
+(#%require sicp-pict)
+
+(define (split first second)
+  (lambda (painter n)
+    (if (= n 0)
+      painter
+      (let ((smaller (up-split painter (- n 1))))
+        (first painter (second smaller smaller))))))
+        
+(define right-split (split beside below))
+(define up-split (split below beside))
+
+
