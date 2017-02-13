5303097bb22146a6e533b9bdcaa0684605641c63
gstraube <gstraube@mailbox.org>
2017-02-13 18:35:16 +0100

    Rotate a painter counterclockwise by 180 and 270 degrees

--- chapter_2/ex250.scm
+++ chapter_2/ex250.scm
@@ -5,3 +5,24 @@
   (transform-painter (make-vect 1.0 0.0)
                      (make-vect 0.0 0.0)
                      (make-vect 1.0 1.0)))
+
+(define rotate90
+  (transform-painter (make-vect 1.0 0.0)
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 0.0)))
+
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
+(define (repeated proc n)
+  (define (iter i result)
+    (if (= i n)
+      result
+      (iter (+ i 1) (compose proc result))))
+  (iter 1 (compose proc (lambda (x) x))))
+
+(define rotate180
+  (repeated rotate90 2))
+
+(define rotate270
+  (repeated rotate90 3))
