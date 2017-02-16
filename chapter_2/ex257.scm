789e0fbbcd6bbbb01af9e2ec7ea9c29bf05e5bb4
gstraube <gstraube@mailbox.org>
2017-02-16 22:51:41 +0100

    Handle an arbitrary number of arguments when forming a sum

---
+++ chapter_2/ex257.scm
@@ -0,0 +1,80 @@
+#lang racket
+
+(define (variable? x) (symbol? x))
+
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
+
+(define (make-sum-all args)
+  (define non-zero-args (filter (lambda (x) (not (=number? x 0))) args))
+  (define (collect-and-sum args numbers non-numbers)
+    (cond ((null? args) (list numbers non-numbers))
+          ((number? (car args)) (collect-and-sum (cdr args) (+ numbers (car args)) non-numbers))
+          (else (collect-and-sum (cdr args) numbers (append non-numbers (list (car args)))))))
+  (collect-and-sum non-zero-args 0 '()))
+
+(define (make-sum a1 a2 . rest)
+  (define result (make-sum-all (append (list a1 a2) rest)))
+  (if (null? (cadr result))
+      (car result)
+      (append (list '+ (car result)) (cadr result))))
+ 
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+
+(define (make-exponentiation base exponent)
+  (cond ((=number? exponent 0) 1)
+        ((=number? exponent 1) base)
+        ((and (number? base) (number? exponent)) (expt base exponent))
+        (else (list '** base exponent))))
+
+(define (sum? x)
+  (and (pair? x) (eq? (car x) '+)))
+
+(define (addend s) (cadr s))
+
+(define (augend s) (caddr s))
+
+(define (product? x)
+  (and (pair? x) (eq? (car x) '*)))
+
+(define (multiplier p) (cadr p))
+
+(define (multiplicand p) (caddr p))
+
+(define (exponentiation? x)
+  (and (pair? x) (eq? (car x) '**)))
+
+(define (base e)
+  (cadr e))
+
+(define (exponent e)
+  (caddr e))
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+           (make-product (multiplier exp)
+                         (deriv (multiplicand exp) var))
+           (make-product (deriv (multiplier exp) var)
+                         (multiplicand exp))))
+        ((exponentiation? exp)
+         (make-product
+          (make-product (exponent exp)
+                        (make-exponentiation (base exp) (- (exponent exp) 1)))
+          (deriv (base exp) var)))
+        (else
+         (error "unknown expression type -- DERIV" exp))))
