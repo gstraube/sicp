0c5597cb6441c37ac7b39c8769a9e9f858ac4833
gstraube <gstraube@mailbox.org>
2017-03-02 20:04:18 +0100

    Rename procedures

--- chapter_2/ex273.scm
+++ chapter_2/ex273.scm
@@ -5,7 +5,7 @@
 (define (make-sum a1 a2) (list '+ a1 a2))
 (define (make-product m1 m2) (list '* m1 m2))
 
-(define (install-make-sum)
+(define (install-sum-rule)
   (define (addend s) (car s))
   (define (augend s) (cadr s))
 
@@ -14,7 +14,7 @@
                (make-sum (deriv (addend exp) var)
                          (deriv (augend exp) var)))))
 
-(define (install-make-product)
+(define (install-product-rule)
   (define (multiplier s) (car s))
   (define (multiplicand s) (cadr s))
 
@@ -26,8 +26,8 @@
                 (make-product (deriv (multiplier exp) var)
                               (multiplicand exp))))))
 
-(install-make-sum)
-(install-make-product)
+(install-sum-rule)
+(install-product-rule)
 
 (define (deriv exp var)
   (define (variable? x) (symbol? x))
