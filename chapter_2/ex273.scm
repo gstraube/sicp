ecad88e9a329b3aeed1e38844481e671edce9dcd
gstraube <gstraube@mailbox.org>
2017-03-03 17:40:05 +0100

    Handle exponentiation in the data-directed derivation procedure

--- chapter_2/ex273.scm
+++ chapter_2/ex273.scm
@@ -4,6 +4,8 @@
 
 (define (make-sum a1 a2) (list '+ a1 a2))
 (define (make-product m1 m2) (list '* m1 m2))
+(define (make-exponentiation base exponent)
+  (list '** base exponent))
 
 (define (install-sum-rule)
   (define (addend s) (car s))
@@ -26,8 +28,20 @@
                 (make-product (deriv (multiplier exp) var)
                               (multiplicand exp))))))
 
+(define (install-power-rule)
+  (define (base e) (car e))
+  (define (exponent s) (cadr s))
+
+  (hash-set! proc-table '(deriv **)
+             (lambda (exp var)
+               (make-product
+                (make-product (exponent exp)
+                              (make-exponentiation (base exp) (- (exponent exp) 1)))
+                (deriv (base exp) var)))))
+
 (install-sum-rule)
 (install-product-rule)
+(install-power-rule)
 
 (define (deriv exp var)
   (define (variable? x) (symbol? x))
