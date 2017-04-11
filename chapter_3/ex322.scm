b439e47f8fde656f6cd842bb7351806aebb1aba3
gstraube <gstraube@mailbox.org>
2017-04-11 21:11:58 +0200

    Enable the insertion of elements

--- chapter_3/ex322.scm
+++ chapter_3/ex322.scm
@@ -5,9 +5,20 @@
        (rear-ptr '()))
   (define (empty?)
    (null? front-ptr))
+  (define (insert-queue! element)
+   (define new-pair (mcons element null))
+   (cond
+    ((empty?)
+     (set! front-ptr new-pair)
+     (set! rear-ptr new-pair))
+    (else
+     (set-mcdr! rear-ptr new-pair)
+     (set! rear-ptr new-pair))))
   (define (dispatch m)
    (cond ((eq? m 'empty?)
 	  (empty?))
+    ((eq? m 'insert-queue!)
+     insert-queue!)
     (else (error "Unknown procedure"))))
   dispatch))
 
